f45a9aa3f0c2b4699e4c0e42033730da1a9fdc35
Felipe Gomez <luisfelipe_1002@hotmail.com>
2024-07-30 15:42:11 -0500

    Implementando estilos

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
 })
-export class AppComponent {
+export class LabsComponent {
   welcome = 'Bienvenido a mi primera aplicación con Angular';
   tasks = [
     'Instalar el Angular CLI',
