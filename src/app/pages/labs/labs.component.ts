df64eeb3aad96664a2c508cbc95e9efdae1b3f5f
Felipe Gomez <luisfelipe_1002@hotmail.com>
2024-08-01 16:36:44 -0500

    Event binding: keydown

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -35,4 +35,9 @@ export class LabsComponent {
     console.log(event);
   }
 
+  keydownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
+
 }
