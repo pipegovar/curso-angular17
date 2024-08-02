dcb7ba64a1a29b3d2381a4544cff456ffbee6568
Felipe Gomez <luisfelipe_1002@hotmail.com>
2024-08-01 19:08:09 -0500

    Creando un Signal en Angular

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -16,7 +16,7 @@ export class LabsComponent {
     'Crear componentes',
     'Crear servicio'
   ]
-  name = 'Felipe';
+  name = signal('Felipe');
   age = 26;
   disabled = true;
   img = 'https://www.w3schools.com/howto/img_avatar.png';
@@ -32,7 +32,9 @@ export class LabsComponent {
   }
 
   changeHandler(event: Event) {
-    console.log(event);
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
   }
 
   keydownHandler(event: KeyboardEvent) {
