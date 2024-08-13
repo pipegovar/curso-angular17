4034ee1b658323375a79482e1bf7db688469fa34
Felipe Gomez <luisfelipe_1002@hotmail.com>
2024-08-13 16:40:16 -0500

    Controlando un input

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
@@ -26,6 +27,14 @@ export class LabsComponent {
     age: 5,
     avatar: 'https://www.w3schools.com/howto/img_avatar.png',
   });
+
+  colorCtrl = new FormControl();
+
+  constructor() {
+    this.colorCtrl.valueChanges.subscribe(value => {
+      console.log(value);
+    })
+  }
 
   clickHandler() {
     alert('Hola');
