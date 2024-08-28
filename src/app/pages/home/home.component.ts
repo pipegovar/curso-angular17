2499f1deaec4fee9c8e456b9d2b4c83bf08febb5
Felipe Gomez <luisfelipe_1002@hotmail.com>
2024-08-28 17:20:50 -0500

    Usando effect para localStorage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, effect, inject, Injector, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
@@ -13,18 +13,8 @@ import { Task } from './../../models/task.model';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tasks = signal<Task[]>([
-    {
-      id: Date.now(),
-      title: 'Crear proyecto',
-      completed: false
-    },
-    {
-      id: Date.now(),
-      title: 'Crear componentes',
-      completed: false
-    },
-  ]);
+
+  tasks = signal<Task[]>([]);
   filter = signal<'all' | 'pending' | 'completed'>('all');
   tasksByFilter = computed(()=> {
     const filter = this.filter();
@@ -44,6 +34,30 @@ export class HomeComponent {
       Validators.required,
     ]
   });
+
+  injector = inject(Injector)
+
+  constructor() {
+
+  }
+
+ngOnInit() {
+  const storage = localStorage.getItem('tasks');
+  if (storage) {
+    const tasks = JSON.parse(storage);
+    this.tasks.set(tasks);
+  }
+  this.trackTasks();
+}
+
+trackTasks () {
+  effect(() => {
+    const tasks = this.tasks();
+    console.log(tasks);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, { injector: this.injector })
+}
+
 
   changeHandler() {
     if (this.newTaskCtrl.valid){
